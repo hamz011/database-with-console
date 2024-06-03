35199d034aaa9fbacd3df5fd26abfb79d7db52e0
hamz011 <154007704+hamz011@users.noreply.github.com>
2024-06-04 02:36:41 +0300

    Add files via upload

---
+++ Database_withConsoel/Database.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Database_withConsoel
+{
+    class Database
+    {
+        public string dbName = "Demo";
+        public SqlConnection connection = new SqlConnection(@"server=(localdb)\MSSQLLocalDB;database=Demo;");
+
+        public SqlDataAdapter adapter;
+        public SqlCommand cmd;
+        public DataTable dataTable;
+        int iddd = 0;
+
+        public void comeData(string sql = "select * from users")
+        {
+            connection.Open();
+            adapter = new SqlDataAdapter(sql, connection);
+            dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            // Print column names
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                Console.Write(column.ColumnName + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------");
+
+            // Print rows data from DataTable
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------------------------------");
+
+        
+
+            Console.WriteLine("İş bitti sorun yok!");
+            connection.Close();
+        }
+
+        public void test()
+        {
+            comeData();
+        }
+
+        public void Add(int id,string name="Hamza",string surname = "deneme")
+        {
+            
+                try
+                {
+                    string sql = "INSERT INTO users (id,name,surname) VALUES (@id,@ad,@soyad)";
+                    cmd = new SqlCommand(sql, connection);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@ad", name);
+                    cmd.Parameters.AddWithValue("@soyad", surname);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                    MessageBox.Show("Yeni kayıt eklendi!\nID: " + id.ToString());
+                }
+                catch (Exception ex)
+                {
+                    connection.Close();
+                    MessageBox.Show("Hata :"+ex.Message);
+                }
+                
+            
+        }
+        public void Update(int id, string name = "Hamza", string surname = "deneme")
+        {
+            if (id < int.MaxValue && id > int.MinValue)
+            {
+                try
+                {
+                    string sql = "UPDATE users set name=@ad,surname=@soyad where id=@id";
+                    cmd = new SqlCommand(sql, connection);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@ad", name);
+                    cmd.Parameters.AddWithValue("@soyad", surname);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                    MessageBox.Show("Kayıt güncellendi!\nID: " + id.ToString() + "\tName: " + name + "\tSurname: " + surname);
+
+                }
+                catch (Exception ex)
+                {
+                    connection.Close();
+                    MessageBox.Show("Hata :" + ex.Message);
+                }
+               
+            }
+            else
+            {
+                MessageBox.Show("Lütfen geçerli ID girin!");
+            }
+        }
+        public void Delete(int id)
+        {
+            if (id < int.MaxValue && id > int.MinValue)
+            {
+                try
+                {
+                    string sql = "DELETE users where id=@id";
+                    cmd = new SqlCommand(sql, connection);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                    MessageBox.Show("Kayıt silindi!\nID: " + id.ToString());
+                }
+                catch (Exception ex)
+                {
+                    connection.Close();
+                    MessageBox.Show("Hata :" + ex.Message);
+                }
+               
+            }
+            else
+            {
+                MessageBox.Show("Lütfen geçerli ID girin!");
+            }
+        }
+    }
+}
---
+++ Database_withConsoel/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Database_withConsoel
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int choose; int aaa = 0;bool answer =false;
+            Database database = new Database();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Console uygulaması ile veritabanı işlemelerine hoş geldiniz!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Veritabanı Bilgileri\nVeritabanı adı: " + database.dbName);
+            do
+            {
+                /*
+                 * 
+                 * 
+                        
+                database.test();
+                Console.WriteLine("Tablo son hali!");
+
+                                                                
+                 
+                 */
+                Console.WriteLine("Yapmak istediğin işlem nedir?\nKayıtları Listeleme --> 1\nYeni kayıt ekleme --> 2\nKayıt silme --> 3\nKayıt güncelleme --> 4\n");
+                choose = int.Parse(Console.ReadLine());
+                switch (choose)
+                {
+                    case 1:
+                        Console.WriteLine("Lütfen bekleyiniz ....");
+                        database.test(); Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Yeni işlem yapmak istiyor musunuz? e/h ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        if (Console.Read().ToString().ToLower() == "e")
+                            answer = true;
+                        else answer = false; break;
+                    case 2:
+                        string nameA = "", surnameA = ""; int idA = aaa;
+                        database.test();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Yukarıda bulunmayan ID li kayıt ekleyin!Lütfen!!"); Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("Yeni kayıt ID'si: ");
+                        string idajhdg = Console.Read().ToString();
+                        if (idajhdg.Length > 0 && idajhdg != string.Empty)
+                        {
+                            idA = int.Parse(idajhdg);
+                        }
+                        else { idA = aaa; aaa++; }
+                        Console.Write("Yeni kayıt Adı: ");
+                        string hgaıjlhgh = Console.Read().ToString();
+                        if (hgaıjlhgh.Length > 0 && hgaıjlhgh != string.Empty)
+                            nameA = hgaıjlhgh;
+                        else
+                        {
+                            Console.WriteLine("Hatalı giriş!");
+                            nameA = "errorHAHAHA";
+                        }
+                        Console.Write("Yeni kayıt Soyadı: ");
+                        string hgaıjlhg= Console.Read().ToString();
+                        if (hgaıjlhg.Length > 0 && hgaıjlhg != string.Empty)
+                            surnameA = hgaıjlhg;
+                        else
+                        {
+                            Console.WriteLine("Hatalı giriş!");
+                            surnameA = "surErrorHAHAHHAHAH";
+                        }
+                        database.Add(idA, nameA, surnameA);
+
+                        database.test();
+                        Console.WriteLine("Tablo son hali!");
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Yeni işlem yapmak istiyor musunuz? e/h" );
+                        Console.ForegroundColor = ConsoleColor.White;
+                        if (Console.ReadLine().ToLower() == "e")
+                            answer = true;
+                        else answer = false;
+
+                        break;
+
+                    case 3:
+                        database.test();
+                        int idD = 0;
+                        Console.WriteLine("Silinecek kaydın ID'sini giriniz: ");
+                        if (Console.ReadLine().Length > 0 && Console.ReadLine() != string.Empty)
+                        {
+                            idD = int.Parse(Console.ReadLine());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Hatalı giriş!");
+                            idD = aaa;
+                        }
+                        database.Delete(idD);
+                        database.test();
+                        Console.WriteLine("Tablo son hali!");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Yeni işlem yapmak istiyor musunuz? e/h" );
+                        Console.ForegroundColor = ConsoleColor.White;
+                        if (Console.Read().ToString().ToLower() == "e")
+                            answer = true;
+                        else answer = false; break;
+                    case 4:
+                        database.test();
+                        string nameU = "", surnameU = ""; int idU = aaa;
+                        Console.Write("Güncellenecek kaydın ID'si: ");
+                        string idd= Console.Read().ToString();
+                        if (idd.Length > 0 && idd != string.Empty)
+                        {
+                            idU = int.Parse(idd);
+                        }
+                        else { idA = aaa; aaa++; }
+                        Console.Write("Kayıtın yeni Adı: ");
+                        string add = Console.Read().ToString();
+                        if (add.Length > 0 && add != string.Empty)
+                            nameU = add;
+                        else
+                        {
+                            Console.WriteLine("Hatalı giriş!");
+                            nameU = "errorHAHAHA";
+                        }
+                        Console.Write("Kayıtın yeni Soyadı: ");
+                        string soy = Console.Read().ToString();
+                        if (soy.Length > 0 && soy!= string.Empty)
+                            surnameU = soy;
+                        else
+                        {
+                            Console.WriteLine("Hatalı giriş!");
+                            surnameU = "surErrorHAHAHHAHAH";
+                        }
+                        database.Update(idU, nameU, surnameU);
+                        database.test();
+                        Console.WriteLine("Tablo son hali!");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Yeni işlem yapmak istiyor musunuz? e/h ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        if (Console.Read().ToString().ToLower() == "e")
+                            answer = true;
+                        else answer = false;
+                        break;
+                    default:
+                        database.test();
+                        Console.WriteLine("Tablo son hali!");
+                        Console.WriteLine("Hatalı giriş!");
+                        break;
+                }
+                Console.WriteLine("Bir şeye bas!");
+                Console.ReadLine();
+
+            }
+            while (answer);
+        }
+
+    }
+}
